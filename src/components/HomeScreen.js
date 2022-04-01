c0bfccb0748f1738a5bfe83221f7d25d2ce2949e
nubol23 <rafael.rvp98@gmail.com>
2022-03-31 23:08:20 -0400

    added logout logic

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../auth/authContext'
+import { authTypes } from '../types/types';
 
 export const HomeScreen = () => {
+
+  const navigate = useNavigate();
+
+  const { userDispatch } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    userDispatch({ type: authTypes.logout })
+
+    navigate('/login', { replace: true });
+  }
+
   return (
-    <div>HomeScreen</div>
+    <>
+      <div>HomeScreen</div>
+      <br />
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </>
   )
 }
