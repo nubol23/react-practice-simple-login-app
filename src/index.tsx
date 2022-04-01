c03bf5bf8b87ab07d1e3baa9c5a2f909c5feecd6
nubol23 <rafael.rvp98@gmail.com>
2022-04-01 18:52:24 -0400

    removed strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
