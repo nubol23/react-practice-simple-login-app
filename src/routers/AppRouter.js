0014001be617ea4533fd773eb39e188f750eca86
nubol23 <rafael.rvp98@gmail.com>
2022-03-31 23:56:22 -0400

    protect home if not authenticated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { LoginScreen } from '../components/LoginScreen'
 import { DashboardRoutes } from './DashboardRoutes'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path='login' element={<LoginScreen />} />
-      <Route path="/*" element={<DashboardRoutes />} />
+      <Route path="/*" element={
+        <PrivateRoute>
+          <DashboardRoutes />
+        </PrivateRoute>
+      } />
     </Routes>
   )
 }
