39126cdd258ede61a37c6dc147018dd5c450a9bc
nubol23 <rafael.rvp98@gmail.com>
2022-03-31 20:44:34 -0400

    added reducer type

--- src/auth/authReducer.ts
+++ src/auth/authReducer.ts
@@ -1,6 +1,18 @@
 import { authTypes } from "../types/types";
 
-export const authReducer = (state = {}, action) => {
+export interface AuthState {
+  accessToken: string;
+  refreshToken: string;
+  issuedAt: number;
+  expiresAt: number;
+  userId: string;
+  firstName: string;
+  email: string;
+  logged: boolean;
+}
+
+
+export const authReducer = (state: AuthState | any = {}, action: any) => {
   switch (action.type) {
     case authTypes.login:
       return {
